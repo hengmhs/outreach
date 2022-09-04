112de9195b017903e44e3b3bbb5d87ab8a8e0069
hengmhs <31433170+hengmhs@users.noreply.github.com>
2022-09-04 18:01:25 +0800

    Added buggy createOutreachDates function

--- src.js
+++ src.js
@@ -1,42 +1,80 @@
+function convertDateFormat(date){
+	let dd = String(date.getDate()).padStart(2, '0');
+	let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0
+	let yyyy = date.getFullYear();
+	date = yyyy + '-' + mm + '-' + dd;
+	return date
+}
+
+function convertDisplayDateFormat(date){
+	let dd = String(date.getDate()).padStart(2, '0');
+	let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0
+	let yyyy = date.getFullYear();
+	date = dd + '/' + mm + '/' + yyyy;
+	return date
+}
+
+function createOutreachDates(date) {
+	// converts string to data object
+	date = new Date(date);
+	outreachDates = [date];
+	for (let step = 0; step < 6; step++) {
+		currentDate = outreachDates[outreachDates.length-1];
+		console.log(step + " currentDate: " + currentDate);
+		// Runs 6 times
+		nextDate = new Date();
+		nextDate.setDate(currentDate.getDate() + 2);
+		console.log(step + " nextDate: " + nextDate);
+		// if sat, add 2 days to get monday
+		// Gets the current day name:  Sunday - Saturday : returns 0 - 6
+		if(nextDate.getDay() === 6){ 
+			nextDate.setDate(nextDate.getDate() + 2) // increments date by 2
+			console.log(step + " Sat nextDate: " + nextDate);
+		}
+		// if sun, add 1 day to get monday
+		if(nextDate.getDay() === 0){
+			nextDate.setDate(nextDate.getDate() + 1)
+			console.log(step + " Sun nextDate: " + nextDate);
+		}
+		outreachDates.push(nextDate);
+	}
+	return outreachDates; 
+}
+
+function convertAllDates(outreachDates){
+	dates = outreachDates.map(convertDisplayDateFormat);
+	return dates;
+}
+
+function updateDateTable(outreachDates){
+	console.log(outreachDates);
+	for (let step = 0; step < 7; step++){
+		document.getElementById("o"+step.toString()).innerHTML = outreachDates[step]
+	}
+}
+
+function dateChange(){
+	date = document.getElementById('start').value;
+	outreachDates = createOutreachDates(date);
+	outreachDates = convertAllDates(outreachDates); // convert dates to dd/mm/yyyy format
+	updateDateTable(outreachDates);
+}
+
+function copyClipboard(){
+	let copyDates = "";
+	for (const date of outreachDates){
+		copyDates += date + "	"; //this blank is a tab so that excel will split it as a delimiter
+	}
+	navigator.clipboard.writeText(copyDates);
+}
+
+// initial setup
+
 let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-
-today = yyyy + '-' + mm + '-' + dd;
-
+today = convertDateFormat(today);
 document.getElementById('start').value = today;
+dateChange();
 
-let test = new Date();
-// Gets the current day name:  Sunday - Saturday : returns 0 - 6
-console.log(test.getDay())
+// change dates when start date is modified
+document.getElementById('start').addEventListener("change", dateChange);
 
-// increments date by 1 
-test.setDate(test.getDate() + 1)
-
-console.log(test.getDay())
-
-// Write a function that takes in a date and returns an array of 7 dates
-
-startDate = new Date();
-outreachDates = [startDate];
-
-let nextDate = startDate
-
-console.log(nextDate);
-
-for (let step = 0; step < 6; step++) {
-	// Runs 6 times
-	nextDate.setDate(nextDate.getDate() + 2)
-	// if sat, add 2 days to get monday
-	if(nextDate.getDay() === 6){
-		nextDate.setDate(nextDate.getDate() + 2)
-	}
-	// if sun, add 1 day to get monday
-	if(nextDate.getDay() === 0){
-		nextDate.setDate(nextDate.getDate() + 1)
-	}
-	outreachDates.push(nextDate);
-  }
-
-  console.log(outreachDates);
